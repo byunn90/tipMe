e581ffccb3b33c2e2463ca6d990d7f4f7b3a57cf
byunn90 <kayhanturk90@Live.com>
2024-01-23 15:32:05 +1100

    tip

---
+++ my-tip-calculator/src/App.js
@@ -0,0 +1,15 @@
+export default function App() {
+  return <Calculator />;
+}
+
+function Tip() {}
+
+function Calculator() {
+  return (
+    <div className="">
+      <h3>How much was the bill?</h3>
+      <input type="text" value="" />
+      <label></label>
+    </div>
+  );
+}
